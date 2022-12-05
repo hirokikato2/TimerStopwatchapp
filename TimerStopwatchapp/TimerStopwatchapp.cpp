2637e3900f484debe4b2e2bbce5125d56ebfad86
Hiroki Kato <kh.258.f.t@i.softbank.jp>
2022-12-05 14:54:25 +0900

    プロジェクト ファイルを追加します。

---
+++ TimerStopwatchapp/TimerStopwatchapp.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<time.h>
+
+int main() {
+
+	clock_t start, end;
+	printf("エンターキーを押すと計測開始");
+
+	getchar();
+	start = clock();
+	printf("エンターキーを押すと計測を終了します");
+
+	getchar();
+	end = clock();
+	printf(
+		"CPU時間(start):%f\nCPU時間(end):%f\n実時間換算：%f seconds\n", (double)start, (double)end, (double)(end - start) / CLOCKS_PER_SEC);
+
+
+	return 0;
+
+};
